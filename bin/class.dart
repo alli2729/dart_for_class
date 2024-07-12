0522274a22fbd6e3b38afef9cef11eac8c5ade4b
Ali Karimi <a.karimi2729@gmail.com>
2024-07-13 01:20:36 +0330

    handeling Wrong guesses

--- bin/class.dart
+++ bin/class.dart
@@ -18,18 +18,25 @@ void main(List<String> args) {
 
     // Game begin with new word after (guess == word)
     while (guess.join('') != word.join('')) {
+      int count = 0;
       // getting input from user
       stdout.write('Enter a Word : ');
       String? input = stdin.readLineSync();
       if (input != null) {
         for (var i = 0; i <= word.length - 1; i++) {
           if (input == word[i]) {
-            guess[i] = input;
             // filling the guess list with right word
+            guess[i] = input;
+          } else {
+            // handeling wrong guesses with stupid and smart logic XD
+            count++;
           }
         }
       }
       print(guess.join(' '));
+      if (count == word.length) {
+        print("Wrong Guess : $input");
+      }
     }
     print('-------------\nWell Done\n-------------\n');
   }
