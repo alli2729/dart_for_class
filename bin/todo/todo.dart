e69c37fe039f5009662865f9fad13f4c0f53e8a4
Ali Karimi <a.karimi2729@gmail.com>
2024-07-30 13:53:36 +0330

    create Todo class

--- bin/todo/todo.dart
+++ bin/todo/todo.dart
@@ -1,20 +1,45 @@
+import 'dart:io';
+
 class Todo {
-  // Variables
+  //----------- Variables
   String title;
   String description;
-  bool status;
   int periority;
+  // defualt value of todo status is pending
+  String status = 'pending';
 
-  // Constractors
+  //----------- Constractors
   Todo(
       {required this.title,
       required this.description,
-      required this.status,
       required this.periority});
 
-  // Methods
+  //----------- Methods
   @override
   String toString() {
-    return 'Title: $title , Description: $description , is Done: $status , priority: $periority';
+    return '$title: $description. =>$status, priority: $periority';
+  }
+
+  // change todo status to DONE
+  void done() {
+    status = 'Done';
+  }
+
+  // change title of todo
+  void editTiltle() {
+    stdout.write('new Title : ');
+    String? input = stdin.readLineSync();
+    if (input != null) {
+      title = input;
+    }
+  }
+
+  // change description of todo
+  void editDescription() {
+    stdout.write('new Description : ');
+    String? input = stdin.readLineSync();
+    if (input != null) {
+      description = input;
+    }
   }
 }
